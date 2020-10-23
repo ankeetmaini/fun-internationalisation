e2c381d81317d6e20836161f2aff3f0fa1109986
Ankeet Maini <ankeet.maini@gmail.com>
2020-10-23 22:00:38 +0530

    new utility to gen type for interpolation

--- generate-types.js
+++ generate-types.js
@@ -9,6 +9,26 @@ const types = [
 
 const allKeys = Object.keys(base);
 
-types.push(`export type AllKeys = ${allKeys.map((k) => `"${k}"`).join(' | ')}`);
+types.push(
+  `export type AllKeys = ${allKeys.map((k) => `"${k}"`).join(' | ')};`,
+);
+
+types.push(`export type GetTranslationTextType<T> = `);
+Object.keys(base).map((key) => {
+  const message = base[key];
+  const occurrences = message.match(/{{[a-zA-Z0-9]+}}/g);
+  const count = occurrences ? occurrences.length : 0;
+  if (count === 0) {
+    types.push(`T extends "${key}" ? never :`);
+  } else {
+    // remove {{ }}
+    const varNames = occurrences
+      .map((v) => v.slice(2, -2))
+      .map((n) => `${n}: string`)
+      .join(',');
+    types.push(`T extends "${key}" ? { ${varNames} } :`);
+  }
+});
+types.push(`never;`);
 
 fs.writeFileSync(path.resolve(__dirname, 'src', 'types.ts'), types.join('\n'));
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,9 @@
-export type AllKeys = "hi" | "coolest" | "greatest" | "discount"
+export type AllKeys = "hi" | "coolest" | "greatest" | "discount";
+
+
+export type GetTranslationTextType<T> = 
+T extends "hi" ? never :
+T extends "coolest" ? never :
+T extends "greatest" ? never :
+T extends "discount" ? { name: string,discount: string } :
+never;
